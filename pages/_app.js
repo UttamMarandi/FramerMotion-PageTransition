bdda7b51e39f1215809cd674bad1a80cd87383c1
Uttam Marandi <uttammarandi007@gmail.com>
2021-09-08 00:52:06 +0530

    AnimatePresense , exitBeforeEnter , exit props

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,22 @@
 import React from "react";
 import App from "next/app";
 import "../style.css";
+import { AnimatePresence } from "framer-motion";
 
 class MyApp extends App {
   render() {
     const { Component, pageProps, router } = this.props;
 
-    return <Component {...pageProps} key={router.route} />;
+    return (
+      <AnimatePresence exitBeforeEnter>
+        <Component {...pageProps} key={router.route} />
+      </AnimatePresence>
+    );
   }
 }
 
 export default MyApp;
+
+// AnimatePresence allows components to animate out when they're removed from the React tree.
+//exitBeforeEnter: boolean
+// If set to true, AnimatePresence will only render one component at a time. The exiting component will finished its exit animation before the entering component is rendered.
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,88 @@
 import Link from "next/link";
 import fetch from "isomorphic-unfetch";
+import { motion } from "framer-motion";
+
+//animate : final state of animation
+//inital : initial state of animation
+//exit : exit props define animation when a component exit
+const easing = [0.6, -0.05, 0.01, 0.99];
+const fadeInUp = {
+  initial: {
+    y: 60,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.6,
+      ease: easing,
+    },
+  },
+};
+
+const stagger = {
+  animate: {
+    transition: {
+      staggerChildren: 0.5,
+    },
+  },
+};
 
 const Index = (props) => (
-  <div>
+  <motion.div
+    exit={{ opacity: 0 }}
+    initial="initial"
+    animate="animate"
+    // initial={{ opacity: 0 }}//this changes the opacity of component
+    // animate={{ opacity: 1 }}
+  >
+    {/*exit props define animation when a component exit  */}
     <div className="container center">
       <div className="title">
         <h1>Select a protein</h1>
       </div>
-      <div className="product-row">
+      <motion.div variants={stagger} className="product-row">
+        {/* parent element should have staggerChidren for the child to stagger */}
         {props.products.map((product) => (
           <Link
             key={product.id}
             href="/products/[id]"
             as={`/products/${product.id}`}
           >
-            <div className="card">
+            <motion.div
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              variants={fadeInUp}
+              className="card"
+            >
               <span className="category">Protein</span>
-              <img key={product.image} src={product.image} width={250} />
+              <motion.img
+                initial={{
+                  x: 60,
+                  opacity: 0,
+                }}
+                animate={{
+                  x: 0,
+                  opacity: 1,
+                }}
+                transition={{
+                  delay: 1,
+                }}
+                key={product.image}
+                src={product.image}
+                width={250}
+              />
               <div className="product-info">
                 <h4>{product.name}</h4>
                 <span>{product.price}</span>
               </div>
-            </div>
+            </motion.div>
           </Link>
         ))}
-      </div>
+      </motion.div>
     </div>
-  </div>
+  </motion.div>
 );
 
 Index.getInitialProps = async function () {
